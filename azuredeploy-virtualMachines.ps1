9123f261e0a88edadb99cbae4498cd5f94402878
matheusdguerra <matheusguerra@outlook.com>
2023-09-08 14:02:01 -0300

    init

---
+++ azuredeploy-virtualMachines.ps1
@@ -0,0 +1,12 @@
+$templateFile="C:\Users\matheus.guerra\OneDrive - GOVERNANCABRASIL S A TECNOLOGIA E GESTAO EM SERVICOS\Área de Trabalho\azure deploy\azuredeploy-virtualMachines.json"
+$today=Get-Date -Format "MM-dd-yyyy"
+$deploymentName="add-virtualMachines-"+"$today"
+$adminUsername="adminUsername"
+$adminPassword="192.168:Gov$"
+
+
+New-AzResourceGroupDeployment `
+  -Name $deploymentName `
+  -TemplateFile $templateFile `
+  -adminUsername $adminUsername `
+  -adminPassword ($adminPassword | ConvertTo-SecureString -AsPlainText -Force) `
